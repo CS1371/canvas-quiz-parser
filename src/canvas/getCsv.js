7f58fd3500559fac2af162cff60fcee24a048bce
Alexander Rao <alexhrao@gmail.com>
2019-02-04 14:00:32 -0500

    Add jsDoc for csv

--- src/canvas/getCsv.js
+++ src/canvas/getCsv.js
@@ -1,4 +1,13 @@
 import fetch from "node-fetch";
+/**
+ * getCsv will return a Promise that resolves to the raw CSV textual data.
+ * This uses the Canvas LMS API - specifically the Quiz Reports section. For
+ * more information, look at the Canvas API Documentation.
+ * @param {string} site The base canvas URL (i.e., institute.instructure.com)
+ * @param {string} course The Course ID
+ * @param {string} quiz The Quiz ID
+ * @param {string} token The Canvas API Token to use
+ */
 export default async function getCsv(site, course, quiz, token) {
     const reportApi = "https://" + site + "/api/v1/courses/" + course + "/quizzes/" + quiz
         + "/reports?quiz_report[report_type]=student_analysis&"
