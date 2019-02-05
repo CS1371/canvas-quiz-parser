6a503d5e11fff105789fd294a6eece4d4673df90
Alexander Rao <alexhrao@gmail.com>
2019-02-05 11:17:49 -0500

    Fix exports from Canvas

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,5 +1,6 @@
 /**
  * This is just a redirect for module importing
  */
+export { default as getQuestions } from "./getQuestions";
 export { default as getStudents } from "./getStudents";
 export { default as getCsv } from "./getCsv";
