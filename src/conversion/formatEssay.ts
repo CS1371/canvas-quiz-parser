14837ddb7a22bd6b31b7b49032b3cd902fd4a38c
Alexander Rao <alexhrao@gmail.com>
2020-03-29 01:30:54 -0400

    Add formatEssay

--- src/conversion/formatEssay.ts
+++ src/conversion/formatEssay.ts
@@ -2,5 +2,5 @@ import { Essay } from "../types/Question";
 
 const formatEssay = (q: Essay, answer: string): string => {
     const { prompt, name } = q;
-    return `<div class="question mfitb"><h2>${name}</h2>${prompt}<pre>${answer}</pre></div>`;
+    return `<div class="question essay"><h2>${name}</h2>${prompt}<pre>${answer}</pre></div>`;
 }
