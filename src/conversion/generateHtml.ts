c3dd63446bca58bd0ca7a003c919c09c3fe853e6
Alexander Rao <alexhrao@gmail.com>
2020-04-24 15:15:31 -0400

    Change prompts to have courier new

--- src/conversion/generateHtml.ts
+++ src/conversion/generateHtml.ts
@@ -30,9 +30,10 @@ const generateHtml = (students: Student[]): string => {
     .cover-page h1, .cover-page p { margin-top: 40px; margin-bottom: 0; } 
     .cover-page h1 { font-size: 200%; } 
     .cover-page p { font-size: 150%; } 
-    .cover-page p.cover-subtitle { font-size: 75%; margin-top: 10px; padding: 0; font-family: 'Courier- New'; }
+    .cover-page p.cover-subtitle { font-size: 75%; margin-top: 10px; padding: 0; font-family: 'Courier New'; }
     .question, .question img { break-after: page; } 
     .question .answer { break-before: page; margin: 20px; }
+    .answer.fitb-answers { font-family: 'Courier New'; }
     .question h2 { margin-bottom: 0; padding-bottom: 0; } 
     .question.essay .answer { min-height: 20in; max-height: 20in; overflow: hidden; } 
     .question-id { font-size: 75%; color: darkgrey; margin: 0; padding: 0; } 
